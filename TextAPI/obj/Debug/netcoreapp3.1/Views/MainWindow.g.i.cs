5ff24353d9d7043b60d267cf6aacfaef9b5492f8
Artem Darkov <darkprothekun@gmail.com>
2021-12-23 02:17:03 +0300

    README.md added

--- TextAPI/obj/Debug/netcoreapp3.1/Views/MainWindow.g.i.cs
+++ TextAPI/obj/Debug/netcoreapp3.1/Views/MainWindow.g.i.cs
@@ -115,7 +115,7 @@ namespace TextAPI.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/TextAPI;component/views/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/TextAPI;V1.0.0.0;component/views/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
